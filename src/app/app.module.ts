33e4ff6f5c6cba0554fc188cabb30cc9b2b0e3d7
Anuj Raj <anuj.dev0311@gmail.com>
2026-09-21 01:50:06 +0530

    added app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './includes/header/header.component';
+import { OfFromComponent } from './includes/of-from/of-from.component';
+import { CustomObservableComponent } from './includes/custom-observable/custom-observable.component';
+import { FilterComponent } from './includes/filter/filter.component';
+import { RetryComponent } from './includes/retry/retry.component';
+import { DebounceComponent } from './includes/debounce/debounce.component';
+import { ConcatMergeComponent } from './includes/concat-merge/concat-merge.component';
+import { SwitchMapComponent } from './includes/switch-map/switch-map.component';
+;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OfFromComponent,
+    CustomObservableComponent,
+    FilterComponent,
+    RetryComponent,
+    DebounceComponent,
+    ConcatMergeComponent,
+    SwitchMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
